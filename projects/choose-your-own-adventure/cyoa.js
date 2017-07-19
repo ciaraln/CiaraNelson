db4ca205fb5264ca451c87fcdd70048338f23ebf
ciara nelson <ciara.l.nelson@live.com>
2017-07-19 19:29:25 -0400

    cyoa

---
+++ projects/choose-your-own-adventure/cyoa.js
@@ -0,0 +1,9 @@
+var response = prompt("Are you seeking adventures in Chattanooga?");
+
+if(response === "No"){
+    response = prompt(" WHAT....!, visit Exploring Chattanooga and start your journey today!");
+} else if (response === "Yes") {
+    response = prompt("Awesome! To seek more adventures, go to Exploring Chattanooga, and continue your journey! ");
+} else {
+    alert("Remember, tomorrow is not promised, start living today. Visit Exploring Chattanooga. :)");
+  }
