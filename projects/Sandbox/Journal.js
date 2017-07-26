fc074ecf12e233661a6ece488a9b0a926afc7eb4
ciara nelson <ciara.l.nelson@live.com>
2017-07-25 21:07:20 -0400

    sandbox

---
+++ projects/Sandbox/Journal.js
@@ -0,0 +1,24 @@
+
+var options = {
+    "font-size": "300%",
+    "margin-left": "200px",
+    "padding": "50px"
+};
+    $("#change-me").animate( options );
+
+$("#everything").css("color", "beige").css("font-wieght", "bolder");
+
+$("body").css("background-color", "salmon");
+$(".holder").css("background-color", "darkgrey");
+$("#everything").css("font-size", "2em");
+
+$(".holder").css("border", "2px solid black");
+$("#everything").css("font-size", "1.1em");
+$("li").css("font-wieght", "bold");
+$("li").css("font-wieght", "bolder").css("font-size", "1.5em");
+$("#secret").css("display", "none");
+$("#change-me").text("Chow for Now!");
+$("#change-me").html("<p>You're Born an Original, Don't Die a Copy by John Mason!</p>");
+("#secret").show()
+("#secret").hide()
+$("p").append("!").prepend("Behold, ");
